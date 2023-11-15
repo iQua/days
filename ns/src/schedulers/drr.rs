4466ba3301f75a7123c4924ee4610af948b72961
Baochun Li <bli@ece.toronto.edu>
2023-11-15 01:30:30 -0500

    Revised the DRRServer design to be more consistent with ns.py.

--- ns/src/schedulers/drr.rs
+++ ns/src/schedulers/drr.rs
@@ -1,62 +1,78 @@
 //! Implements a Deficit Round Robin (DRR) server.
 
-use std::collections::{HashMap, VecDeque};
-use std::cmp::min_by;
 use crate::packets::packet::Packet;
-use sim::{Sender, Receiver, Time, channel};
+use sim::{channel, Receiver, Sender, Time};
+use std::cmp::min_by;
+use std::collections::{HashMap, VecDeque};
 
 pub struct DRRServer {
     element_id: u32,
-    // the bit rate of the port
+    /// the bit rate of the port
     rate: f64,
-    // a HashMap for weights of flows
-    weights: HashMap<String, u32>,
-    // the number of packets sent
-    packets_sent: u32,
-    // the number of packets received
-    packets_received: u32,
-    // the number of packets of each flow
-    packets_in_queue: HashMap<u32, u32>,
-    // the bytes of each flow
-    bytes_in_queue: HashMap<u32, u32>,
-    // the quantum counter for all flows
+    /// a hash map: class_id -> weight
+    weights: HashMap<u32, u32>,
+
+    /// a closure that maps a flow_id to a class_id, used to implement
+    /// class-based Deficit Round Robin. The default uses a packet's flow_id as
+    /// its class_id, which is equivalent to flow-based DRR.
+    pub flow_classes: Box<dyn Fn(u32) -> u32>,
+
+    /// class_id -> deficit
+    deficit: HashMap<u32, u32>,
+    /// class_id -> the number of packets in its queue
+    flow_queue_count: HashMap<u32, u32>,
+    /// class_id -> quantum
     quantum: HashMap<u32, u32>,
-    // the deficit counter for all flows
-    deficits: HashMap<u32, u32>,
-    // one FIFO queue for each flow
+    /// class_id -> the head-of-line packet in its queue
+    head_of_line: HashMap<u32, Packet>,
+
+    /// the number of packets received
+    packets_received: u32,
+    /// the current packet being sent to the downstream element, if any
+    current_packet: Option<Packet>,
+    /// class_id -> the number of bytes in its queue
+    byte_sizes: HashMap<u32, u32>,
+
+    // class_id -> its FIFO queue
     queues: HashMap<u32, VecDeque<(Packet, Time)>>,
-    // a sender for sending packets
+
+    /// a sender for sending packets to downstream elements
     pub sender: Sender<Packet>,
-    /// a receiver for receiving incoming packets
+    /// a receiver for receiving incoming packets from upstream elements
     pub receiver: Receiver<Packet>,
 }
 
 impl DRRServer {
-    pub fn new(element_id: u32, rate: f64, weights: HashMap<String, u32>) -> DRRServer {
+    pub fn new(element_id: u32, rate: f64, weights: HashMap<u32, u32>) -> DRRServer {
         let min_quantum = 1500;
-        let min_weight = weights.values().min_by(|a, b| a.cmp(b)).unwrap_or(&1);
-        let quantum: HashMap<u32, u32> = weights.iter()
-            .filter_map(|(key, &value)| {
-                key.parse::<u32>().ok().map(|parsed_key| (parsed_key, min_quantum * value / min_weight))
-            })
-            .collect();
+        let mut deficit = HashMap::new();
+        let mut quantum = HashMap::new();
+        let mut flow_queue_count = HashMap::new();
+
+        let min_weight = weights.values().min().unwrap();
+        for (class_id, weight) in &weights {
+            deficit.insert(*class_id, 0);
+            quantum.insert(*class_id, min_quantum * weight / min_weight);
+            flow_queue_count.insert(*class_id, 0);
+        }
+
         println!("weights: {:?};\nquantum: {:?}", weights, quantum);
+
         DRRServer {
             element_id,
             rate,
             weights,
-            packets_sent: 0,
-            packets_received: 0,
-            packets_in_queue: HashMap::new(),
-            bytes_in_queue: HashMap::new(),
+            flow_classes: Box::new(|flow_id| flow_id),
+            deficit: deficit,
+            flow_queue_count: flow_queue_count,
             quantum: quantum,
-            deficits: HashMap::new(),
+            head_of_line: HashMap::new(),
+            packets_received: 0,
+            current_packet: None,
+            byte_sizes: HashMap::new(),
             queues: HashMap::new(),
             sender: channel().0,
-            receiver: channel().1
+            receiver: channel().1,
         }
     }
-
-    
-
 }
