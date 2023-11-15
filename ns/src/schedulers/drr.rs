a171ef4ada7fc127d1c38f6ea69e0c0ed4fbc3cc
kai <kai.shen@mail.utoronto.ca>
2023-11-15 04:02:44 +0000

    Modified the new function for the DRRServer.

--- ns/src/schedulers/drr.rs
+++ ns/src/schedulers/drr.rs
@@ -1,6 +1,7 @@
 //! Implements a Deficit Round Robin (DRR) server.
 
 use std::collections::{HashMap, VecDeque};
+use std::cmp::min_by;
 use crate::packets::packet::Packet;
 use sim::{Sender, Receiver, Time, channel};
 
@@ -10,8 +11,20 @@ pub struct DRRServer {
     rate: f64,
     // a HashMap for weights of flows
     weights: HashMap<String, u32>,
-    // the packet queue of the server
-    queue: VecDeque<(Packet, Time)>,
+    // the number of packets sent
+    packets_sent: u32,
+    // the number of packets received
+    packets_received: u32,
+    // the number of packets of each flow
+    packets_in_queue: HashMap<u32, u32>,
+    // the bytes of each flow
+    bytes_in_queue: HashMap<u32, u32>,
+    // the quantum counter for all flows
+    quantum: HashMap<u32, u32>,
+    // the deficit counter for all flows
+    deficits: HashMap<u32, u32>,
+    // one FIFO queue for each flow
+    queues: HashMap<u32, VecDeque<(Packet, Time)>>,
     // a sender for sending packets
     pub sender: Sender<Packet>,
     /// a receiver for receiving incoming packets
@@ -20,13 +33,30 @@ pub struct DRRServer {
 
 impl DRRServer {
     pub fn new(element_id: u32, rate: f64, weights: HashMap<String, u32>) -> DRRServer {
+        let min_quantum = 1500;
+        let min_weight = weights.values().min_by(|a, b| a.cmp(b)).unwrap_or(&1);
+        let quantum: HashMap<u32, u32> = weights.iter()
+            .filter_map(|(key, &value)| {
+                key.parse::<u32>().ok().map(|parsed_key| (parsed_key, min_quantum * value / min_weight))
+            })
+            .collect();
+        println!("weights: {:?};\nquantum: {:?}", weights, quantum);
         DRRServer {
             element_id,
             rate,
             weights,
-            queue: VecDeque::new(),
+            packets_sent: 0,
+            packets_received: 0,
+            packets_in_queue: HashMap::new(),
+            bytes_in_queue: HashMap::new(),
+            quantum: quantum,
+            deficits: HashMap::new(),
+            queues: HashMap::new(),
             sender: channel().0,
             receiver: channel().1
         }
     }
+
+    
+
 }
