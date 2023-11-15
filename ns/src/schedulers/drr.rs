b1b33d822aff42815b6a7768eddffab90299393d
kai <kai.shen@mail.utoronto.ca>
2023-11-15 23:06:11 +0000

    Deleted RefCells.

--- ns/src/schedulers/drr.rs
+++ ns/src/schedulers/drr.rs
@@ -3,7 +3,7 @@
 use crate::packets::packet::Packet;
 use crate::Shared;
 use sim::{channel, select, until, Control, Receiver, Sender, SimContext, Time};
-use std::{collections::{HashMap, VecDeque}, cell::RefCell};
+use std::collections::{HashMap, VecDeque};
 use futures::future::join;
 
 pub struct DRRServer {
@@ -17,26 +17,26 @@ pub struct DRRServer {
     pub flow_classes: Box<dyn Fn(u32) -> u32>,
 
     /// class_id -> deficit
-    deficit: RefCell<HashMap<u32, u32>>,
+    deficit: HashMap<u32, u32>,
     /// class_id -> the number of packets in its queue
-    flow_queue_count: RefCell<HashMap<u32, u32>>,
+    flow_queue_count: HashMap<u32, u32>,
     /// class_id -> quantum
-    quantum: RefCell<HashMap<u32, u32>>,
+    quantum: HashMap<u32, u32>,
     /// class_id -> the head-of-line packet in its queue
-    head_of_line: RefCell<HashMap<u32, Packet>>,
+    head_of_line: HashMap<u32, Packet>,
 
     /// the number of packets received
-    packets_received: RefCell<u32>,
+    packets_received: u32,
     // the total number of packets in the server
-    total_packets: RefCell<Control<u32>>,
+    total_packets: Control<u32>,
     /// the current packet being sent to the downstream element with its sending
     /// time, if any
-    current_packet: RefCell<Option<(Packet, Time)>>,
+    current_packet: Option<(Packet, Time)>,
     /// class_id -> the number of bytes in its queue
-    byte_sizes: RefCell<HashMap<u32, u32>>,
+    byte_sizes: HashMap<u32, u32>,
 
     /// class_id -> its FIFO queue
-    queues: RefCell<HashMap<u32, VecDeque<(Packet, Time)>>>,
+    queues: HashMap<u32, VecDeque<(Packet, Time)>>,
 
     /// a sender for sending packets to downstream elements
     pub sender: Sender<Packet>,
@@ -47,20 +47,20 @@ pub struct DRRServer {
 impl DRRServer {
     pub fn new(element_id: u32, rate: f64, weights: HashMap<u32, u32>) -> DRRServer {
         let min_quantum = 1500;
-        let deficit = RefCell::new(HashMap::new());
-        let quantum = RefCell::new(HashMap::new());
-        let flow_queue_count = RefCell::new(HashMap::new());
-        let byte_sizes = RefCell::new(HashMap::new());
+        let mut deficit = HashMap::new();
+        let mut quantum = HashMap::new();
+        let mut flow_queue_count = HashMap::new();
+        let mut byte_sizes = HashMap::new();
 
         let min_weight = weights.values().min().unwrap();
         for (class_id, weight) in &weights {
-            deficit.borrow_mut().insert(*class_id, 0);
-            quantum.borrow_mut().insert(*class_id, min_quantum * weight / min_weight);
-            flow_queue_count.borrow_mut().insert(*class_id, 0);
-            byte_sizes.borrow_mut().insert(*class_id, 0);
+            deficit.insert(*class_id, 0);
+            quantum.insert(*class_id, min_quantum * weight / min_weight);
+            flow_queue_count.insert(*class_id, 0);
+            byte_sizes.insert(*class_id, 0);
         }
 
-        println!("weights: {:?};\nquantum: {:?}", weights, quantum.borrow());
+        println!("weights: {:?};\nquantum: {:?}", weights, quantum);
         // TODO: add the usage of flow_classes!
         DRRServer {
             element_id,
@@ -69,34 +69,31 @@ impl DRRServer {
             deficit,
             flow_queue_count,
             quantum,
-            head_of_line: RefCell::new(HashMap::new()),
-            packets_received: RefCell::new(0),
-            total_packets: RefCell::new(Control::default()),
-            current_packet: RefCell::new(None),
+            head_of_line: HashMap::new(),
+            packets_received: 0,
+            total_packets: Control::default(),
+            current_packet: None,
             byte_sizes,
-            queues: RefCell::new(HashMap::new()),
+            queues: HashMap::new(),
             sender: channel().0,
             receiver: channel().1,
         }
         // Q: do we need packet_available?
     }
 
-    fn packet_received(&self, packet: Packet, sim: SimContext<'_, Shared>) {
+    fn packet_received(&mut self, packet: Packet, sim: SimContext<'_, Shared>) {
         self.queues
-            .borrow_mut()
             .entry(packet.flow_id)
             .or_insert_with(VecDeque::new)
             .push_back((packet.clone(), sim.now()));
 
-        *self.packets_received.borrow_mut() += 1;
-        self.total_packets.borrow_mut().set(self.total_packets.borrow().get() + 1);
+        self.packets_received += 1;
+        self.total_packets.set(self.total_packets.get() + 1);
 
         self.byte_sizes
-            .borrow_mut()
             .entry(packet.flow_id)
             .and_modify(|byte_size| *byte_size += packet.size);
         self.flow_queue_count
-            .borrow_mut()
             .entry(packet.flow_id)
             .and_modify(|flow_id| *flow_id += 1);
 
@@ -108,34 +105,30 @@ impl DRRServer {
             packet.size,
             packet.flow_id,
             sim.now(),
-            self.packets_received.borrow(),
-            self.flow_queue_count.borrow().get(&packet.flow_id).unwrap(),
-            self.total_packets.borrow().get()
+            self.packets_received,
+            self.flow_queue_count.get(&packet.flow_id).unwrap(),
+            self.total_packets.get()
         );
     }
 
-    fn packet_sent(&self, packet: Packet, sim: SimContext<'_, Shared>) {
-        self.total_packets.borrow_mut().set(self.total_packets.borrow().get() - 1);
+    fn packet_sent(&mut self, packet: Packet, sim: SimContext<'_, Shared>) {
+        self.total_packets.set(self.total_packets.get() - 1);
 
         self.byte_sizes
-            .borrow_mut()
             .entry(packet.flow_id)
             .and_modify(|byte_size| {
                 *byte_size -= packet.size;
             });
         self.flow_queue_count
-            .borrow_mut()
             .entry(packet.flow_id)
             .and_modify(|packet_count| *packet_count -= 1);
 
         self.deficit
-            .borrow_mut()
             .entry(packet.flow_id)
             .and_modify(|deficit| *deficit -= packet.size);
 
-        if *self.flow_queue_count.borrow().get(&packet.flow_id).unwrap() == 0 {
+        if *self.flow_queue_count.get(&packet.flow_id).unwrap() == 0 {
             self.deficit
-                .borrow_mut()
                 .entry(packet.flow_id)
                 .and_modify(|deficit| *deficit = 0);
         }
@@ -148,57 +141,52 @@ impl DRRServer {
             packet.size,
             packet.flow_id,
             sim.now(),
-            self.flow_queue_count.borrow().get(&packet.flow_id).unwrap(),
-            self.total_packets.borrow().get()
+            self.flow_queue_count.get(&packet.flow_id).unwrap(),
+            self.total_packets.get()
         );
     }
 
-    pub async fn fetch_packet(&self, sim: SimContext<'_, Shared>) {
-        println!("Inside fetch_packet function.");
+    pub async fn fetch_packet(&mut self, sim: SimContext<'_, Shared>) {
         loop {
             // wait until there exist packets
-            // until(&（self.total_packets.borrow()）, |counts| counts.get() >
-            // 0).await;
+            until(&self.total_packets, |counts| counts.get() > 0).await;
 
-            until(&(*self.total_packets.borrow()), |counts| counts.get() > 0).await;
-
-            for (flow_id, &count) in self.flow_queue_count.borrow().iter() {
+            for (flow_id, &count) in self.flow_queue_count.iter() {
                 if count > 0 {
-                    self.deficit.borrow_mut().entry(*flow_id).and_modify(|deficit| {
-                        *deficit += self.quantum.borrow_mut().get(flow_id).unwrap();
+                    self.deficit.entry(*flow_id).and_modify(|deficit| {
+                        *deficit += self.quantum.get(flow_id).unwrap();
                     });
                 }
 
-                while *self.deficit.borrow().get(flow_id).unwrap() > 0
-                    && *self.flow_queue_count.borrow().get(flow_id).unwrap() > 0
+                while *self.deficit.get(flow_id).unwrap() > 0
+                    && *self.flow_queue_count.get(flow_id).unwrap() > 0
                 {
                     let packet; // Do we need to store the arrival time here?
-                    if let Some(head_packet) = self.head_of_line.borrow_mut().remove(flow_id) {
+                    if let Some(head_packet) = self.head_of_line.remove(flow_id) {
                         packet = head_packet;
                     } else {
-                        packet = self.queues.borrow_mut().get_mut(flow_id).unwrap().pop_front().unwrap().0;
+                        packet = self.queues.get_mut(flow_id).unwrap().pop_front().unwrap().0;
                     }
 
-                    if packet.size < *self.deficit.borrow().get(flow_id).unwrap() {
+                    if packet.size < *self.deficit.get(flow_id).unwrap() {
                         let wait_time = (packet.size as f64) * 8.0 / self.rate;
-                        *self.current_packet.borrow_mut() = Some((packet, wait_time));
+                        self.current_packet = Some((packet, wait_time));
                         sim.advance(wait_time).await;
 
                         // updates stats will be done in run funtion
                     } else {
-                        self.head_of_line.borrow_mut().insert(*flow_id, packet);
+                        self.head_of_line.insert(*flow_id, packet);
                     }
                 }
             }
         }
     }
 
-    pub async fn schedule_packets(&self, sim: SimContext<'_, Shared>) {
-        println!("Inside schedule_packet function.");
+    pub async fn schedule_packets(&mut self, sim: SimContext<'_, Shared>) {
         loop {
             let receive_action = self.receiver.recv();
             let send_action = async {
-                if let Some((_, wait_time)) = self.current_packet.borrow().clone() {
+                if let Some((_, wait_time)) = self.current_packet.clone() {
                     sim.advance(wait_time).await;
                 } else {
                     sim.advance(1.0).await;
@@ -210,7 +198,7 @@ impl DRRServer {
                     self.packet_received(packet, sim);
                 }
                 None => {
-                    if let Some((packet, _)) = self.current_packet.borrow_mut().take() {
+                    if let Some((packet, _)) = self.current_packet.take() {
                         self.sender
                             .send(packet.clone())
                             .await
@@ -223,10 +211,9 @@ impl DRRServer {
         }
     }
 
-    pub async fn run(self, sim: SimContext<'_, Shared>) {
-        println!("Inside run function.");
-        let fetch_task = self.fetch_packet(sim);
-        let schedule_task = self.schedule_packets(sim);
-        let _ = join(fetch_task, schedule_task).await;
+    pub async fn run(mut self, sim: SimContext<'_, Shared>) {
+        let fetch_action = self.fetch_packet(sim);
+        let schedule_action = self.schedule_packets(sim);
+        let _ = join(fetch_action, schedule_action);
     }
 }
