0b9f14ef6aef691064dbcaa827ae2b6e4c2a03f8
Baochun Li <bli@ece.toronto.edu>
2023-11-14 19:24:05 -0500

    Reformatted the code.

--- ns/examples/port.rs
+++ ns/examples/port.rs
@@ -6,12 +6,12 @@ use rand::{rngs::SmallRng, SeedableRng};
 use rand_distr::{Exp, Uniform};
 use std::cell::RefCell;
 
-use ns::packets::packet::Packet;
 use ns::packets::dist_generator::DistPacketGenerator;
+use ns::packets::packet::Packet;
 use ns::packets::sink::PacketSink;
 use ns::ports::port::Port;
 use ns::Shared;
-use sim::{channel, Process, RandomVar, SimContext, Sender, Receiver};
+use sim::{channel, Process, RandomVar, Receiver, Sender, SimContext};
 
 const SEED: u64 = 1000;
 
@@ -31,12 +31,12 @@ async fn network_sim(sim: SimContext<'_, Shared>) {
             Box::new(|| Exp::new(1.).unwrap()),
             Box::new(|| Uniform::new(1000, 1001)),
         );
-        generator.sender = senders[i as usize/2].clone();
+        generator.sender = senders[i as usize / 2].clone();
         sim.activate(generator.run(sim));
     }
-    
-    let mut port_on_packets = Port::new(0, (1000*8) as f64, 1, false);
-    let mut port_on_bytes = Port::new(1, (1000*8) as f64, 1000, true);
+
+    let mut port_on_packets = Port::new(0, (1000 * 8) as f64, 1, false);
+    let mut port_on_bytes = Port::new(1, (1000 * 8) as f64, 1000, true);
     let mut sink = PacketSink::new(0);
 
     port_on_packets.receiver = receivers[0].clone();
@@ -45,7 +45,7 @@ async fn network_sim(sim: SimContext<'_, Shared>) {
 
     port_on_packets.sender = senders[2].clone();
     port_on_bytes.sender = senders[2].clone();
-    
+
     sim.activate(port_on_packets.run(sim));
     sim.activate(port_on_bytes.run(sim));
     sim.activate(sink.run(sim));
