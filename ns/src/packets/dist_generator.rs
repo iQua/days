d2f87877a4c9426ec2c040a288b2e34a9d1628eb
kai <kai.shen@mail.utoronto.ca>
2023-11-16 22:27:33 +0000

    Revised the packet sending logic in dist_generator.

--- ns/src/packets/dist_generator.rs
+++ ns/src/packets/dist_generator.rs
@@ -65,6 +65,11 @@ where
         sim.advance(self.initial_delay).await;
 
         while sim.now() < sim.shared().duration {
+            
+            // wait for the next transmission
+            let interval = (self.arr_interval_dist)().sample(&mut *sim.shared().rng.borrow_mut());
+            sim.advance(interval).await;
+
             let packet_size = (self.packet_size_dist)().sample(&mut *sim.shared().rng.borrow_mut());
 
             let packet = Packet {
@@ -84,8 +89,6 @@ where
 
             self.packet_sent(sim, packet);
 
-            let interval = (self.arr_interval_dist)().sample(&mut *sim.shared().rng.borrow_mut());
-            sim.advance(interval).await;
         }
     }
 }
