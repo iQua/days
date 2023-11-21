34ffaa84e2f42f29f234cb24f41e92079d3dc16b
Ningxin Su <ningxin.su@mail.utoronto.ca>
2023-11-21 17:00:05 -0500

    Revert "Fixed a typo in print info."
    This reverts commit a2de43ba10d8724ee0e23ac392887846e6f5bc58.

--- due/examples/drr.rs
+++ due/examples/drr.rs
@@ -92,7 +92,7 @@ fn main() {
     );
 
     println!(
-        "Statistics on queuing delay in this simulation: {:#.3}",
+        "Statistics on queueing delay in this simulation: {:#.3}",
         outcome.queueing_delay
     );
 }
