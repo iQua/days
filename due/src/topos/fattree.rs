70cc6ac7c163c7e8f412a07d615cf61c2e99c6af
Kai Shen <kai.shen@mail.utoronto.ca>
2023-11-23 03:08:20 +0000

    Modified the input of FatTree, /src/topos/fattree.rs not done  yet.

--- due/src/topos/fattree.rs
+++ due/src/topos/fattree.rs
@@ -1,4 +1,4 @@
-use std::io::Sink;
+
 
 use statrs::statistics::Distribution;
 
@@ -6,11 +6,12 @@ use crate::packets::dist_generator::DistPacketGenerator;
 use crate::packets::sink::PacketSink;
 use crate::sim::{SimContext, Time};
 use crate::switches::switch::PacketSwitch;
+use crate::Shared;
 
 pub struct FatTree<A, B>
 where
-    A: Distribution<Time>,
-    B: Distribution<f64>,
+    A: Distribution<Time> + 'static,
+    B: Distribution<f64> + 'static,
 {
     k: usize,
     /// packet generators in hosts
@@ -27,17 +28,39 @@ where
 
 impl<A, B> FatTree<A, B>
 where
-    A: Distribution<Time>,
-    B: Distribution<f64>,
+    A: Distribution<Time> + 'static,
+    B: Distribution<f64> + 'static,
 {
-    pub fn new(k: usize) -> FatTree<A, B> {
+    pub fn new(k: usize, generators: Vec<DistPacketGenerator<A, B>>, sinks: Vec<PacketSink>, core_switches: Vec<PacketSwitch>, agg_switches: Vec<PacketSwitch>, edge_switches: Vec<PacketSwitch>) -> FatTree<A, B> {
+        assert!(k > 0 && k % 2 == 0, "Invalid k!");
         FatTree {
             k,
-            generators: Vec::new(),
-            sinks: Vec::new(),
-            core_switches: Vec::new(),
-            agg_switches: Vec::new(),
-            edge_switches: Vec::new(),
+            generators,
+            sinks,
+            core_switches,
+            agg_switches,
+            edge_switches,
         }
+    }
+
+    pub async fn run(self, sim: SimContext<'_, Shared>) {
+        for generator in self.generators {
+            sim.activate(generator.run(sim));
+        }
+        for sink in self.sinks {
+            sim.activate(sink.run(sim));
+        }
+        for switch in self.core_switches {
+            sim.activate(switch.run(sim));
+        }
+        for switch in self.agg_switches {
+            sim.activate(switch.run(sim));
+        }
+        for switch in self.edge_switches {
+            sim.activate(switch.run(sim));
+        }
+
+        // waits for the end of this simulation
+        sim.advance(sim.shared().duration + 100.).await;
     }
 }
