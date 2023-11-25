b165e2dd8d2f77331456c077223d605e6147dee2
Baochun Li <bli@ece.toronto.edu>
2023-11-25 09:00:27 -0500

    Cleaned up basic_topo.rs.

--- due/examples/basic_topo.rs
+++ due/examples/basic_topo.rs
@@ -3,6 +3,7 @@
 //! distribution, and then to a packet sink.
 
 use std::cell::RefCell;
+use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
 
 use petgraph::graph::UnGraph;
@@ -14,16 +15,15 @@ use due::packets::source::PacketSource;
 use due::packets::wire::Wire;
 use due::sim::{simulation, Process, RandomVar, SimContext};
 use due::topos;
-use due::topos::connect_n_1_hetero;
-use due::{Element, Shared};
+use due::{Shared, Sink, Source};
 
 const SEED: u64 = 1000;
 
 async fn network_sim(sim: SimContext<'_, Shared>) {
     let graph = UnGraph::<i32, ()>::from_edges(&[(0, 1)]);
 
-    let mut sources: Vec<Box<dyn Element>> = Vec::new();
-    let mut sinks: Vec<Box<dyn Element>> = Vec::new();
+    let mut sources: Vec<Box<dyn Source>> = Vec::new();
+    let mut sinks: Vec<Box<dyn Sink>> = Vec::new();
 
     let arr_interval_dist = Arc::new(|| Exp::new(1.0).unwrap());
     let packet_size_dist = Arc::new(|| DiscreteUniform::new(1000, 1500).unwrap());
@@ -36,49 +36,17 @@ async fn network_sim(sim: SimContext<'_, Shared>) {
     }
 
     // creates a sink
-    let mut sink = Sink::default();
+    let mut sink = PacketSink::default();
     sinks.push(Box::new(sink));
 
     // creates a wire that can be used to connect elements in the network
     let mut wire = Wire::new(0, Box::new(|| Uniform::new(2.0, 2.0).unwrap()));
 
-    // constructs the network graph
-    topos::construct_graph(graph, &mut sources, &mut sinks, &mut wire);
+    // constructs the network graph with the (optionally provided) wire
+    topology = topos::construct(graph, &mut wire);
 
-    // attaches sources and sinks to the wire in the graph
-    for node_index in g.node_indices() {
-        match g[node_index].node_type {
-            NodeType::Source | NodeType::Sink => {
-                for &edge_node in &edge_node_idxs {
-                    g.add_edge(node_index, edge_node, ());
-                }
-            }
-            _ => {}
-        }
-    }
-
-    // connects neighbor nodes through connect function
-    for node_idx in g.node_indices() {
-        match g[node_idx].node_type {
-            NodeType::Edge | NodeType::Sink => {
-                let mut upstream_ids = Vec::new();
-                let mut upstreams: Vec<Box<&mut dyn Element>> = Vec::new();
-                for neighbor_idx in g.neighbors_undirected(node_idx) {
-                    upstream_ids.push(g[neighbor_idx].id);
-                }
-                for upstream_id in upstream_ids {
-                    if let Some(element) = elements.get_mut(&upstream_id) {
-                        upstreams.push(Box::new(element.as_mut()));
-                    }
-                }
-                let downstream = elements.get_mut(&g[node_idx].id).unwrap();
-                connect_n_1_hetero(&mut upstreams, downstream.as_mut());
-            }
-            _ => {
-                continue;
-            }
-        }
-    }
+    // attaches sources and sinks to hosts in the network graph
+    topology.attach(sources, sinks);
 
     // waits for the end of this simulation
     sim.advance(sim.shared().duration + 100.).await;
