6dd62bad0ef7a6c2a83ec404023418a43cc58bee
Chen Ying <c.ying@mail.utoronto.ca>
2023-11-27 10:08:34 -0500

    Changed to use one queue for all the classes instead of one queue for one class.

--- due/src/schedulers/wfq.rs
+++ due/src/schedulers/wfq.rs
@@ -59,9 +59,11 @@ pub struct WFQServer {
     /// a closure that determines whether an inbound packet should be dropped or not
     drop_strategy: Box<dyn PacketDrop>,
 
+    weights: Vec<f64>,
     /// finish time of the last packet served in each class
     finish_times: Vec<f64>,
-
+    flow_queue_count: Vec<usize>,
+    active_set: Vec<usize>,
     vtime: f64,
     last_update: f64,
 
@@ -73,8 +75,8 @@ pub struct WFQServer {
     /// the number of bytes of classes, which are consecutive and start from 0
     byte_sizes: Vec<usize>,
 
-    /// priority queues of classes, where packets are sorted according to their finish times
-    queues: Vec<BinaryHeap<TaggedPacket>>,
+    /// priority queue of packets from all the classes, where packets are sorted according to their finish times
+    scheduler_queue: BinaryHeap<TaggedPacket>,
 
     /// a sender for sending outbound packets to the downstream element
     pub sender: UnboundedSender<Packet>,
@@ -99,19 +101,21 @@ impl WFQServer {
         capacity_unit: CapacityUnit,
         flow_classes: Arc<dyn Fn(usize) -> usize>,
         drop_strategy: DropStrategy,
-        weights: Vec<usize>,
+        weights: Vec<f64>,
     ) -> WFQServer {
         let mut finish_times = Vec::new();
+        let mut flow_queue_count = Vec::new();
+        let mut active_set = Vec::new();
         let mut vtime = 0.0;
         let mut last_update = 0.0;
         let mut byte_sizes = Vec::new();
-        let mut queues = Vec::new();
+        let mut scheduler_queue = BinaryHeap::new();
         let (sender, receiver) = unbounded_channel();
 
         for (class_id, _) in weights.iter().enumerate() {
             finish_times.push(0.0);
+            flow_queue_count.push(0);
             byte_sizes.push(0);
-            queues.push(BinaryHeap::new());
         }
 
         let packet_drop = match drop_strategy {
@@ -124,14 +128,17 @@ impl WFQServer {
             rate,
             flow_classes,
             drop_strategy: Box::new(packet_drop),
+            weights,
             finish_times,
+            flow_queue_count,
+            active_set,
             vtime,
             last_update,
             packets_received: 0,
             packets_dropped: 0,
             packets_waiting: 0,
             byte_sizes,
-            queues,
+            scheduler_queue,
             sender,
             receiver,
         }
@@ -142,7 +149,7 @@ impl WFQServer {
         let should_drop_packet = self.drop_strategy.should_drop(
             packet.packet.size,
             self.byte_sizes.iter().sum(),
-            self.queues.iter().map(|q| q.len()).sum(),
+            self.scheduler_queue.len(),
         );
 
         // the case that this packet will be dropped.
@@ -163,20 +170,50 @@ impl WFQServer {
 
         let class_id = (self.flow_classes)(packet.packet.flow_id);
 
-        self.queues[class_id].push(packet.clone());
+        self.scheduler_queue.push(packet.clone());
+
         self.byte_sizes[class_id] += packet.packet.size;
 
         println!(
             "WFQServer {} received packet {} ({} bytes) from flow {} at time {:.3}. \
-            {} packets received, {} packet(s) in class queue {}.",
+            {} packets received, {} packet(s) in queue {}.",
             self.scheduler_id,
             packet.packet.packet_id,
             packet.packet.size,
             packet.packet.flow_id,
             now,
             self.packets_received,
-            self.queues[class_id].len(),
+            self.scheduler_queue.len(),
             class_id
         );
+    }
+
+    fn update_stats(&mut self, packet: TaggedPacket, now: Time) {
+        let mut weight_sum = 0.0;
+
+        // updates the virtual time based on the current set of active flow classes
+        for i in self.active_set {
+            weight_sum += self.weights[i];
+        }
+
+        self.vtime += (now - self.last_update) / weight_sum;
+
+        // computes the new set of active flow classes
+        let class_id = (self.flow_classes)(packet.packet.flow_id);
+
+        self.flow_queue_count[class_id] -= 1;
+        if self.flow_queue_count[class_id] == 0 {
+            let index = self.active_set.iter().position(|x| *x == class_id).unwrap();
+            self.active_set.remove(index);
+        }
+
+        if self.active_set.is_empty() {
+            self.vtime = 0.0;
+            self.finish_times[class_id] = 0.0;
+        }
+
+        self.last_update = now;
+
+        self.byte_sizes[class_id] -= packet.packet.size;
     }
 }
