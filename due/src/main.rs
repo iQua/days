18445b7d6175e3b9a3b696d3a33b9c83cf12c859
Kai Shen <kai.shen@mail.utoronto.ca>
2023-11-27 22:40:02 +0000

    Modified attach_to in main.rs.

--- due/src/main.rs
+++ due/src/main.rs
@@ -29,7 +29,7 @@ async fn network_sim(config_path: String, sim: SimContext<'_, Shared>) {
     // constructs the network graph with network elements
     topology.connect();
     // attaches sources and sinks to hosts in the network graph
-    topology.attach(vec![0, 0, 1, 1]);
+    topology.attach(vec![0, 1, 1, 0]);
     // computes shortest paths for all flows, and sets fibs for all switches
     topology.set(flows, sim);
     // runs the topology
