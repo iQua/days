77a92f4445e6d1824e4270ee54b3d6623c164f47
Kai Shen <kai.shen@mail.utoronto.ca>
2023-12-03 05:44:57 +0000

    Minor revision to examples/new_core.rs.

--- due/examples/new_core.rs
+++ due/examples/new_core.rs
@@ -10,29 +10,6 @@ use due::flows::flow::Flow;
 use due::topos::topo::Topology;
 use due::{get_seed, Shared};
 
-// async fn network_sim(config_path: &str, sim: Simulator<Shared>) {
-//     let file_path = config_path;
-
-//     let graph = UnGraph::<usize, ()>::from_edges([(0, 1)]);
-//     let hosts = vec![0, 1];
-//     info!("The network graph has been initialized: {:?}", graph);
-
-//     let flows = Flow::flows_from_config(file_path);
-//     debug!(
-//         "A total of {} network flows has been initialized.",
-//         flows.len()
-//     );
-
-//     // initializes the topology
-//     let topology = Topology::new(file_path, graph, hosts, flows);
-
-//     // // runs the topology
-//     topology.run(sim);
-
-//     // // waits for the end of this simulation
-//     sim.advance(sim.shared().duration + 100.).await;
-// }
-
 #[tokio::main]
 async fn main() {
     let env = env_logger::Env::default();
