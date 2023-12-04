b9965f2cf9838d5b61d1e4659dd3d8afe19d81f2
Kai Shen <kai.shen@mail.utoronto.ca>
2023-12-04 06:30:06 +0000

    Revised main.rs.

--- due/src/main.rs
+++ due/src/main.rs
@@ -1,19 +1,18 @@
 //! The main program for running a simulation using a specific configuration.
 
 use std::env;
-use std::sync::Mutex;
+use std::sync::Arc;
 
 use log::{debug, info};
 use petgraph::graph::UnGraph;
-use rand::{rngs::SmallRng, SeedableRng};
 
 use due::flows::flow::Flow;
-use due::sim::{simulation, Process, RandomVar, SimContext};
+use due::sim::{RandomVar, Simulator};
 // use due::topos::build::build_graph;
 use due::topos::topo::Topology;
 use due::{get_seed, Shared};
 
-async fn network_sim(config_path: String, sim: SimContext<'_, Shared>) {
+async fn network_sim(config_path: String, sim: Arc<Simulator<Shared>>) {
     let file_path = config_path.as_str();
 
     // There are three ways of building a network graph:
@@ -55,13 +54,11 @@ async fn network_sim(config_path: String, sim: SimContext<'_, Shared>) {
     let topology = Topology::new(file_path, graph, hosts, flows);
 
     // runs the topology
-    topology.run(sim);
-
-    // waits for the end of this simulation
-    sim.advance(sim.shared().duration + 100.).await;
+    topology.run(Arc::clone(&sim)).await;
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let env = env_logger::Env::default();
     env_logger::init_from_env(env);
 
@@ -73,17 +70,17 @@ fn main() {
     let path = args[1].clone();
     let seed = get_seed(&path);
 
-    let outcome = simulation(
-        Shared {
-            rng: Mutex::new(SmallRng::seed_from_u64(seed)),
-            queueing_delay: RandomVar::new(),
-            duration: 1.5,
-        },
-        |sim| Process::new(sim, network_sim(path, sim)),
-    );
+    let shared = Shared {
+        queueing_delay: RandomVar::new(),
+        duration: 10.0,
+    };
 
-    println!(
-        "Statistics on queueing delay in this simulation: {:#.3}",
-        outcome.queueing_delay
-    );
+    let sim = Simulator::new(shared, seed);
+    sim.run(Arc::clone(&sim), network_sim(path, Arc::clone(&sim)))
+        .await;
+
+    // println!(
+    //     "Statistics on queueing delay in this simulation: {:#.3}",
+    //     outcome.queueing_delay
+    // );
 }
