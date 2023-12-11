32f17edcc03ba54b8185c8a006b73e2024d82ae4
Kai Shen <kai.shen@mail.utoronto.ca>
2023-12-11 16:56:38 +0000

    Modified basic.rs to run the simulation based on 1 thread.

--- due/examples/basic.rs
+++ due/examples/basic.rs
@@ -39,7 +39,7 @@ fn main() {
 
     // Instantiates the simulator.
     let t0 = MonotonicTime::EPOCH;
-    let mut sim = SimInit::new()
+    let mut sim = SimInit::with_num_threads(1)
         .add_model(source, source_mbox)
         .add_model(sink, sink_mbox)
         .init(t0);
