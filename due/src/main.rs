512d9888f410f3b6dcbd095138aea32d894e41e7
Baochun Li <bli@ece.toronto.edu>
2023-12-17 21:17:41 -0500

    Minor changes in main.rs.

--- due/src/main.rs
+++ due/src/main.rs
@@ -61,15 +61,15 @@ fn main() {
 
     // 1. initializes collectives directly using collectives_from_graph().
     //    Example:
-    let collectives = Collective::collectives_from_graph(
-        vec![vec![(0, 1), (0, 2)]],
-        vec![vec![0]],
-        vec![vec![1, 2]],
-    );
+    // let collectives = Collective::collectives_from_graph(
+    //     vec![vec![(0, 1), (0, 2)]],
+    //     vec![vec![0]],
+    //     vec![vec![1, 2]],
+    // );
 
     // 2. initializes flows using a configuration file.
     //    Example:
-    // let collectives = Collective::collectives_from_config(file_path);
+    let collectives = Collective::collectives_from_config(file_path);
 
     info!(
         "A total of {} collective communication operations has been initialized.",
