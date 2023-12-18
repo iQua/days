1860308fe080b491b7f5222666e78bebaefca26f
Baochun Li <bli@ece.toronto.edu>
2023-12-17 21:29:39 -0500

    Fixed a bug on seeding the packet source.

--- due/src/flows/source.rs
+++ due/src/flows/source.rs
@@ -60,10 +60,11 @@ impl PacketSource {
         seed: usize,
     ) -> PacketSource {
         let global_seed = get_seed();
-        let rng = match seed {
+        let rng = match global_seed {
             1.. => SmallRng::seed_from_u64((global_seed + seed) as u64),
             _ => SmallRng::from_entropy(),
         };
+        println!("PacketSource seed: {}", (global_seed + seed) as u64);
 
         PacketSource {
             endpoint_id: next_endpoint_id(),
