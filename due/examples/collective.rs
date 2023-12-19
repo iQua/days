a6462f1e26077db0ac62df4fac830e8d3db6b051
Chen Ying <c.ying@mail.utoronto.ca>
2023-12-19 12:25:58 -0500

    Added an example of running collective communication primitives.

---
+++ due/examples/collective.rs
@@ -0,0 +1,33 @@
+//! An example of running collective communication primitives.
+
+use log::info;
+
+use due::flows::collective::Collective;
+use due::seed_from_config;
+use due::topos::build::build_graph;
+use due::topos::topo::Topology;
+
+fn main() {
+    let env = env_logger::Env::default();
+    env_logger::init_from_env(env);
+
+    let file_path = "configs/collective.toml";
+    let _ = seed_from_config(&file_path);
+
+    let graph = build_graph(file_path);
+    let hosts = vec![0, 1, 2, 3, 4, 5];
+    info!("The network graph has been initialized: {:?}", graph);
+
+    let collectives = Collective::collectives_from_config(file_path);
+
+    info!(
+        "A total of {} collective communication operations has been initialized.",
+        collectives.len()
+    );
+
+    // initializes the topology
+    let topology = Topology::new(file_path, graph.clone(), hosts, Vec::new(), collectives);
+
+    // runs the topology
+    topology.run(graph);
+}
