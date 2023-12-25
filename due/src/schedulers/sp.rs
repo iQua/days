90f603330924009e6b791794a71f3343a15bfb49
Chen Ying <c.ying@mail.utoronto.ca>
2023-12-25 17:16:42 -0500

    Updated comments.

--- due/src/schedulers/sp.rs
+++ due/src/schedulers/sp.rs
@@ -22,8 +22,8 @@ pub struct SPServer {
     rate: f64,
 
     /// a closure that maps a flow_id to a class_id, used to implement
-    /// class-based Deficit Round Robin. The default uses a packet's flow_id as
-    /// its class_id, which is equivalent to flow-based DRR.
+    /// class-based Static Priority. The default uses a packet's flow_id as
+    /// its class_id, which is equivalent to flow-based SP.
     pub flow_classes: Arc<dyn Fn(usize) -> usize + Send + Sync>,
 
     /// a closure that determines whether an inbound packet should be dropped or not
@@ -148,7 +148,7 @@ impl SPServer {
         self.output.send(packet).await;
     }
 
-    /// Moves on to the next non-empty priority queue if the current queue is empty
+    /// Moves on to the next non-empty priority queue if the current queue is empty.
     fn next_priority(&mut self) -> Option<usize> {
         for (&priority, queue) in self.queues.iter() {
             if !queue.is_empty() {
