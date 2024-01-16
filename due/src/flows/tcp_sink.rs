e64f05d7e20050b67c9671cee1ae6dfde10c94f0
Chen Ying <c.ying@mail.utoronto.ca>
2024-01-16 10:05:51 -0500

    Finished implementing TCPSink.

--- due/src/flows/tcp_sink.rs
+++ due/src/flows/tcp_sink.rs
@@ -8,7 +8,7 @@ use log::debug;
 use asynchronix::model::{Model, Output};
 use asynchronix::time::{MonotonicTime, Scheduler};
 
-use crate::flows::packet::Packet;
+use crate::flows::packet::{Packet, TCPAck};
 use crate::flows::sink::{PacketStatistics, RandomVar};
 use crate::next_endpoint_id;
 
@@ -28,6 +28,11 @@ pub struct TCPPacketSink {
     queueing_delays: RandomVar,
     /// the size of the packets
     packet_sizes: RandomVar,
+    /// the receive buffer, which is a priority queue that is sorted based on
+    /// the sequence number of the packet (packet_id)
+    recv_buffer: Vec<(usize, usize)>,
+    /// the next sequence number expected to be received
+    next_seq_expected: usize,
     /// output: packet statistics
     pub statistics: Output<PacketStatistics>,
     /// output: outbound to packet switches
@@ -45,6 +50,8 @@ impl TCPPacketSink {
             one_way_delays: RandomVar::new(),
             queueing_delays: RandomVar::new(),
             packet_sizes: RandomVar::new(),
+            recv_buffer: Vec::new(),
+            next_seq_expected: 0,
             statistics: Output::default(),
             output: Output::default(),
         }
@@ -76,7 +83,7 @@ impl TCPPacketSink {
         self.statistics.send(self.statistics()).await;
     }
 
-    pub fn packet_received(&mut self, packet: Packet, scheduler: &Scheduler<Self>) {
+    pub async fn packet_received(&mut self, packet: Packet, scheduler: &Scheduler<Self>) {
         let now = scheduler.time();
         let arrival_time = now.duration_since(MonotonicTime::EPOCH).as_secs_f64();
         self.arrival_times.tabulate(arrival_time);
@@ -91,6 +98,57 @@ impl TCPPacketSink {
         debug!(
             "TCPPacketSink {} received packet {} ({} bytes) from flow {} at time {:.3}.",
             self.endpoint_id, packet.packet_id, packet.size, packet.flow_id, arrival_time,
+        );
+
+        // inserts the packet into the receive buffer and sorts based on the
+        // sequence number of the packet (packet_id)
+        let sequence_num = packet.packet_id;
+        self.recv_buffer
+            .push((sequence_num, sequence_num + packet.size));
+        self.recv_buffer.sort();
+
+        let mut merged_stats: Vec<(usize, usize)> = Vec::new();
+        for (start, end) in self.recv_buffer.iter() {
+            if merged_stats.last().is_some() & (start <= &merged_stats.last().unwrap_or(&(0, 0)).1)
+            {
+                let last = merged_stats.last_mut().unwrap();
+                *last = (last.0, *end.max(&last.1));
+            } else {
+                merged_stats.push((*start, *end));
+            }
+        }
+
+        self.recv_buffer = merged_stats;
+
+        if self.recv_buffer.len() == 1 {
+            // in-order delivery: all data up to but not including
+            // `next_seq_expected` have been received
+            self.next_seq_expected = packet.packet_id + packet.size;
+        } else {
+            // out-of-order delivery or retransmissions: needs to go through the
+            // receive buffer and find out what the last in-order packet's
+            // sequence number is
+            self.next_seq_expected = self.recv_buffer[0].1;
+        }
+
+        let acknowledgment = Packet {
+            time: packet.time,
+            creation_time: packet.creation_time,
+            size: 40,
+            packet_id: packet.packet_id,
+            flow_id: packet.flow_id,
+            queueing_delay: packet.queueing_delay,
+            ack: Some(TCPAck {
+                sequence_num: self.next_seq_expected,
+            }),
+        };
+
+        // sends the Ack packet out to the TCPPacketSource now
+        self.output.send(acknowledgment.clone()).await;
+
+        debug!(
+            "TCPPacketSink {} sent Ack packet {} ({} bytes) at time {:.3}.",
+            self.endpoint_id, acknowledgment.packet_id, acknowledgment.size, arrival_time,
         );
     }
 }
