9fce4fa2fc65030cfbf3e89cade5b443b524e8f1
Chen Ying <c.ying@mail.utoronto.ca>
2024-02-10 22:41:48 -0500

    Improved a comments.

--- due/src/flows/source.rs
+++ due/src/flows/source.rs
@@ -141,7 +141,7 @@ impl PacketSource {
                         .duration_since(MonotonicTime::EPOCH)
                         .as_secs_f64();
 
-                    // AppDataSource sends data at this point, update statistics
+                    // AppDataSource sends data at this point, updates statistics
                     // about its traffic production
                     source.datasource.data_sent(&data, now);
 
