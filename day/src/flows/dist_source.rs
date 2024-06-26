ee62805d00611629b841cf858f283aaa7d335f4c
Baochun Li <bli@ece.toronto.edu>
2024-06-26 17:06:00 -0400

    Updated cargo package versions.

--- day/src/flows/dist_source.rs
+++ day/src/flows/dist_source.rs
@@ -86,7 +86,11 @@ impl DistPacketSource {
                 .sample(&mut self.rng),
             DistributionInfo::Exp { lambda } => Exp::new(lambda).unwrap().sample(&mut self.rng),
             DistributionInfo::Uniform { low, high } => {
-                Uniform::new(low, high).unwrap().sample(&mut self.rng)
+                if low == high {
+                    low
+                } else {
+                    Uniform::new(low, high).unwrap().sample(&mut self.rng)
+                }
             }
         };
 
@@ -99,7 +103,11 @@ impl DistPacketSource {
                 Exp::new(lambda).unwrap().sample(&mut self.rng) as usize
             }
             DistributionInfo::Uniform { low, high } => {
-                Uniform::new(low, high).unwrap().sample(&mut self.rng) as usize
+                if low == high {
+                    low as usize
+                } else {
+                    Uniform::new(low, high).unwrap().sample(&mut self.rng) as usize
+                }
             }
         };
 
