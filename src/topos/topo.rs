5dc2af6deb4e77a35cbd36867d01b4a86df566c2
Chen Ying <c.ying@mail.utoronto.ca>
2024-07-06 16:19:38 -0400

    Supported custom path from a configuration file.

--- src/topos/topo.rs
+++ src/topos/topo.rs
@@ -247,6 +247,7 @@ impl Topology {
                     CollectiveType::Broadcast => {
                         self.flows.push(Flow::new(
                             flow_id,
+                            None,
                             Vec::new(),
                             Vec::new(),
                             collective.flow_type,
@@ -267,6 +268,7 @@ impl Topology {
                     CollectiveType::Gather => {
                         self.flows.push(Flow::new(
                             flow_id,
+                            None,
                             Vec::new(),
                             Vec::new(),
                             collective.flow_type,
@@ -286,6 +288,7 @@ impl Topology {
                     CollectiveType::AllReduce => {
                         self.flows.push(Flow::new(
                             flow_id,
+                            None,
                             Vec::new(),
                             Vec::new(),
                             collective.flow_type,
