333a0e1823c741fa2efc0bf3693c7154020bc13e
YirenZzz <zhaoyirendaisy@icloud.com>
2025-05-04 23:00:03 -0400

    Added support for creating TCPPacketSource using shared packet vector.

--- src/flows/source.rs
+++ src/flows/source.rs
@@ -61,7 +61,8 @@ impl PacketSource {
         flow_start_after: Vec<usize>,
         flow_type: FlowType,
         traffic: TrafficCharacteristics,
-        seed: usize,
+        // seed: usize,
+        packets: Vec<Packet>,
     ) -> Self {
         let global_seed = get_seed();
         let rng = match global_seed {
@@ -80,7 +81,8 @@ impl PacketSource {
                 flow_id,
                 flow_start_after,
                 traffic,
-                rng,
+                // rng,
+                packets,
             )),
         }
     }
