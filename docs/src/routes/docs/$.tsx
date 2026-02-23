d161531b2da5c66914ed61e8c2755a85baf393f0
Baochun Li <bli@ece.toronto.edu>
2026-02-23 11:13:49 -0500

    Added Mermaid support to docs rendering.

--- docs/src/routes/docs/$.tsx
+++ docs/src/routes/docs/$.tsx
@@ -8,6 +8,7 @@ import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { baseOptions } from '@/lib/layout.shared';
 import { useFumadocsLoader } from 'fumadocs-core/source/client';
 import { Suspense } from 'react';
+import { Mermaid } from '@/components/mdx/mermaid';
 
 export const Route = createFileRoute('/docs/$')({
   component: Page,
@@ -49,6 +50,7 @@ const clientLoader = browserCollections.docs.createClientLoader({
           <MDX
             components={{
               ...defaultMdxComponents,
+              Mermaid,
             }}
           />
         </DocsBody>
